34b5254997e61c3a090fd0c442d60268858fb0f9
Alex Turner <none>
2023-03-14 10:22:47 +0000

    text edits

--- main.py
+++ main.py
@@ -3,12 +3,21 @@ import streamlit as st
 def the_nursing_formula():
     ''' the ‘nursing formula': volume to administer  = dose required (what you want) * volume of solution (what you got)
      / solution strength (what is in it)’'''
+    # intro block
+    st.title('The Nursing Formula')
+    st.write("""
+Universal Formula (Toney-Butler, 2021)
+In the universal formula (or “desired over have method”), the desired amount (D) is the dose  prescribed and the amount 
+on hand (H) or the amount you “have” is the available dose or concentration. The quantity (Q) is the form and amount in 
+which the drug is supplied (i.e. tablet, capsule, liquid). To calculate the dose, take the desired amount and divide it 
+by the amount on hand, then multiply it by the quantity.    
+    """)
 
     # inputs block
     medication = st.text_input('Name of medication:')
-    dose_required = st.number_input('Amount prescribed (mg):')
-    vol_solution = st.number_input('Volume of solution (ml):')
-    dose_available = st.number_input('Solution strength (mg/ml?):')
+    dose_required = st.number_input('the desired amount (D) (mg):')
+    vol_solution = st.number_input('the quantity (Q) (ml):')
+    dose_available = st.number_input('available dose or concentration (H) (mg/ml?):')
 
     # the nursing formula calculation
     if dose_available != 0:
@@ -21,7 +30,7 @@ def the_nursing_formula():
 
     # output block
     st.subheader('THE NURSING FORMULA')
-    form = f'{dose_required}mg (what you want) x {vol_solution}ml (what you got) / {dose_available}mg (what\'s in it)'
+    form = f'{dose_required}mg (what you want) x {vol_solution}ml (what you have) / {dose_available}mg (what\'s in it)'
     st.write(form)
 
     st.write(f'Volume to administer = {vol_to_administer}ml')
